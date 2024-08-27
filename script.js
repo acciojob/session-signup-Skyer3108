9ae76eecbaefff6acfc95b64bd7e20d565170f96
Skyer3108 <136808389+Skyer3108@users.noreply.github.com>
2024-08-27 19:05:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 //your JS code here. If required.
+
+document.getElementById('signup-form').addEventListener('submit',(e)=>{
+
+	e.preventDefault();
+
+	const name=document.getElementById('name').value;
+	const email=document.getElementById('email').value;
+	const password=document.getElementById('password').value;
+	const conPassword=document.getElementById('confirm-password').value;
+
+	if(password===conPassword){
+
+		sessionStorage.setItem('name',name)
+		sessionStorage.setItem('email',email)
+		sessionStorage.setItem('password',password)
+		
+
+		
+	}
+	else{
+		alert('Password do not match')
+	}
+	
+})
